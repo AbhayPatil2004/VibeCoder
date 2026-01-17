21e416b171b4d4c6c51fe4696f3b5f05c6be2407
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2026-01-17 17:05:53 +0530

    prisma setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Button  } from "@/components/ui/button";
+import {db} from '@/lib/db'
 
-export default function Home() {
+export default async function Home() {
+
+  const user = db.user
   return (
     <div>
+      <div>{user}</div>
       <Button>
         Get Started
-        Hello World
       </Button>
     </div>
   );
