75a74c9441c24c331f0bda63f005e03c39965bd6
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2026-01-18 16:21:29 +0530

    next auth setup cokmpleted

---
+++ auth.config.js
@@ -0,0 +1,17 @@
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
+const authConfig = {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+}
+
+export default authConfig
