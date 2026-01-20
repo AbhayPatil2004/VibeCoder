dc28ce6ee71ff3ec3d0042e9af45ca4a01d336ce
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2026-01-20 17:22:42 +0530

    landing page completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./provider";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,12 +20,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* <Toaster /> */}
+
+            {children}
+
+          </ThemeProvider>
         </Providers>
       </body>
     </html>
